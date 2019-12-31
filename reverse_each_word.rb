884a8b5926fe647ef731936e96ef5d07728d06a9
Doak Covington <doakcovington@protonmail.com>
2019-12-31 04:38:43 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -6,3 +6,12 @@ def reverse_each_word(string)
   end #end of word loop
   new_array.join(" ")
 end #end of reverse_each_word
+
+#reverse_each_word method
+def reverse_each_word(string)
+  new_array = string.split(" ")
+  new_array.collect do |word|
+    word.reverse!
+  end #end of word loop
+  new_array.join(" ")
+end #end of reverse_each_word
